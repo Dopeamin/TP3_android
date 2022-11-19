1a56daea1e256cafe83daa665308e78e73ebf924
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-11-19 09:25:28 +0100

    finishes ex2

--- app/src/main/java/com/example/tp3/MainActivity.kt
+++ app/src/main/java/com/example/tp3/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.tp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import com.example.tp3.databinding.ActivityMainBinding
 import layout.FragmentClock
@@ -15,13 +18,29 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.switch1.isChecked = true;
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment,FragmentClock(),null)
             .addToBackStack(null)
             .commit()
     }
 
-    public fun setTime(view : View)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_resource_file, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_switch)
+        {
+            binding.switch1.isChecked = !binding.switch1.isChecked
+            setTime(null)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    public fun setTime(view: View?)
     {
         var fragmentManager = supportFragmentManager
         var transaction = fragmentManager.beginTransaction()
